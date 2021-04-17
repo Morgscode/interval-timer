3a014ff459c875d12ed12ffaafa26804ad1a2071
Luke Morgan <lukemorgan@Lukes-Mac-mini.lan>
2021-04-17 19:19:57 +0100

    adds api authentication

---
+++ app/Http/Controllers/Auth/ApiAuthController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Http\Rsponse;
+
+use Illuminate\Support\Facades\Hash;
+
+use App\Models\User;
+
+class ApiAuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $user_login = $this->validateLoginCredentials($request);
+
+        $user = User::where('email', $user_login['email'])->firstOrFail();
+
+         //check password
+         if ( ! $user || ! Hash::check($user_login['password'], $user['password'])) :
+            return response([
+                'message' => 'incorrect login credentials',
+                'status_code' => 401
+            ], 401);
+        endif;
+
+        if ($user->hasVerifiedEmail()) {
+
+            $token = $user->createToken('ItApiAccessToken');
+        
+            return [
+                'user' => $user,
+                'token' => $token->plainTextToken
+            ];
+
+        } else {
+
+            return [
+                'status' => 'Not Authorized',
+                'status_code' => 401,
+                'message' => 'Please verify your email address before ussing this service'
+            ];
+        }
+    }
+
+    public function logout(Request $request)
+    {
+        auth()->user()->tokens()->delete();
+        return [
+            'message' => 'Successfully logged out',
+            'status_code' => 200
+        ];
+    }
+
+    public function validateLoginCredentials(Request $request)
+    {
+        $valid_login_attempt = $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string'
+        ]);
+
+        return $valid_login_attempt;
+    }
+}
